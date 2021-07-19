fad432ce3cdafa363882046ac0027417c9104498
kolchanov <kolchanov@siberian.pro>
2021-07-19 17:15:43 +0000

    Базовая конфигурация бота и его опции:)

---
+++ bot/index.js
@@ -0,0 +1,14 @@
+﻿const ms = require('millisecond')
+const { Telegraf } = require('telegraf')
+const config = require('config')
+
+const bot = new Telegraf(config.get('botToken'), {
+    handlerTimeout: ms('5s'),
+    telegram: { webhookReply: false }
+})
+
+module.exports = bot
+
+Object.defineProperty(bot.context, "botInfo", {
+    get() { return bot.botInfo }
+})
