0edafcb8256a78ba55391dcda6e0e6adfbc86a06
kolchanov <kolchanov@siberian.pro>
2021-07-27 13:40:48 +0000

    Команда help, ничего примечательного

---
+++ handlers/commands/help.js
@@ -0,0 +1,13 @@
+﻿const StartMessage = require('../../stores/context/start.js')
+
+const helpHandler = async ctx => {
+    if (ctx.chat.type !== 'private') return
+    const message = await StartMessage.getCurrentStart()
+    if (!message) {
+        ctx.reply("В базе данных не выбран start")
+        return 
+    }
+    return ctx.replyWithHTML(message.data)
+}
+
+module.exports = helpHandler
