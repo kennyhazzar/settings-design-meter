e3033e9ad94ab0b41698fde091da7e059bcf8883
kolchanov <kolchanov@siberian.pro>
2021-07-27 13:41:45 +0000

    Основной файл папки commands

---
+++ handlers/commands/index.js
@@ -0,0 +1,13 @@
+﻿const { Composer } = require('telegraf')
+
+const commands = new Composer()
+
+const start = require('./start.js')
+const help = require('./help.js')
+const chat = require('./settings/chats.js')
+
+commands.command('/chat', chat)
+commands.start(start)
+commands.help(help)
+
+module.exports = commands
