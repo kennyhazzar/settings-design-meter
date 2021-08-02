534850f7b316128636ab7acb90569b010316e6a6
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:17:02 +0000

    Обработчик остального текста

--- handlers/middlewares/index.js
+++ handlers/middlewares/index.js
@@ -4,8 +4,9 @@ const middlewares = new Composer()
 
 const photoHandler = require('./photo.js')
 const addUpdate = require('./addUpdate.js')
-
+const messageHandler = require('./messageText.js')
 middlewares.on('photo', photoHandler)
 middlewares.use(addUpdate)
+middlewares.on('text', messageHandler)
 
 module.exports = middlewares
