fe8311be979a46fa2072668541a5dc5deb853503
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:11:17 +0000

    Сборщик пустых сообщений

---
+++ stores/context/voidMessage.js
@@ -0,0 +1,52 @@
+﻿const Datastore = require('nedb')
+
+const nedbPromise = require('nedb-promise')
+
+const db = new Datastore({autoload: true, filename: 'data/context/EmptyMessage.db'})
+
+const Message = nedbPromise.fromInstance(db)
+
+const compactDb = () => db.persistence.compactDatafile()
+
+const isMessageValid = (messageText) => typeof messageText == 'string' ? true : false
+
+const getMessageOne = (query = {}) => Message.findOne(query)
+
+const getMessage = (query = {}) => Message.find(query)
+
+const getCurrentMessage = () => Message.findOne({ isSelect: true })
+
+const addMessage = (messageText, isSelect = false) =>
+    ismessageValid(messageText) ?
+        Message.insert({ data: messageText, isSelect }) : null
+
+
+const updateCurrentmessage = (newMessageText) => {
+    ismessageValid(newMessageText)
+        ? Message.update({ isSelect: true }, { $set: { data: newMessageText } }) : null
+    compactDb()
+}
+
+const changeCurrentMessage = idNewCurrent => {
+    Message.update(
+        { isSelect: true },
+        { $set: { isSelect: false } }
+    )
+    Message.update({ _id: idNewCurrent }, { $set: { isSelect: true } })
+    compactDb()
+}
+
+const removeMessage = (_id) => {
+    Message.remove({ _id })
+    compactDb()
+}
+
+module.exports = {
+    getCurrentMessage,
+    getMessage,
+    getMessageOne,
+    addMessage,
+    updateCurrentmessage,
+    changeCurrentMessage,
+    removeMessage
+}
