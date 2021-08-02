952f3976ff6b69dcd23fa32104315fb156679268
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:14:44 +0000

    Ответ на остальной текст

---
+++ handlers/middlewares/messageText.js
@@ -0,0 +1,10 @@
+﻿const MessageText = require('../../stores/context/voidMessage.js')
+
+const messageHandler = ctx => {
+    if (ctx.chat.type !== 'private') return null
+    const message = MessageText.getCurrentMessage()
+    if (Object.keys(message).length == 0) return ctx.reply("Бот не настроен: ответ на текст.")
+    ctx.reply(message.data)
+}
+
+module.exports = messageHandler
