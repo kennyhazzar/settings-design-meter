6b8f9b64983520b24d30ec5da9a308330249f667
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:07:28 +0000

    Команда для обновления информации текущего шаблона

---
+++ handlers/commands/settings/start/updateStart.js
@@ -0,0 +1,20 @@
+﻿const Start = require('../../../../stores/context/start.js')
+
+const updateStartHandler = async ctx => {
+    var userMessage = ctx.message.text
+
+    const commandInstance = userMessage.substring(userMessage.search(' '), userMessage.length).trim()
+    if (commandInstance === '/updateStart') {
+        const start = await Start.getCurrentStart()
+        ctx.reply("Для обновления текущего приветственного сообщения, введи /updateStart <Текст сообщения>" +
+            "\n\nТекущее сообщение:\n" + (start?.data ? start.data : "Не выбрано"))
+    } else {
+        const start = await Start.getCurrentStart()
+        if (!start?.data) return ctx.reply("Невозможно обновить текущее приветственное сообщение:\n" +
+            "Текущее приветственное сообщение не выбрано")
+        Start.updateCurrentStart(commandInstance)
+        ctx.reply("Изменения внесены.")
+    }
+}
+
+module.exports = updateStartHandler
