ca72b8918b39f0da57441e83492f4566026046d8
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:07:00 +0000

    Команда, которая показывает все шаблоны в бд и текущий выбранный, если такой есть

---
+++ handlers/commands/settings/start/showStarts.js
@@ -0,0 +1,19 @@
+﻿const Start = require('../../../../stores/context/start.js')
+
+const showStartHandler = async ctx => {
+    var userMessage = ctx.message.text
+
+    const start = await Start.getStart()
+    const currentStartData = (await Start.getCurrentStart())?.data
+
+
+    const commandInstance = userMessage.substring(userMessage.search(' '), userMessage.length).trim()
+    if (commandInstance === '/showStarts') {
+        ctx.reply(`Список шаблонов приветственного сообщения:\n
+        ${start.length == 0 ? "Пусто\n" : start.map((item, index) => {
+            return "\n" + `${index + 1}. ${item.data}\n`
+        }).join('')}\n${currentStartData ? "Текущий выбор:\n" + commandInstance : ""}`)
+    }
+}
+
+module.exports = showStartHandler
