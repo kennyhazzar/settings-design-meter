5a685a2a154f9de4aaee081e3cc3f9930bed8c91
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:09:49 +0000

    Добавлены функции для получения апдейтов из бд

--- stores/logs/getUpdatesLog.js
+++ stores/logs/getUpdatesLog.js
@@ -1,13 +1,19 @@
 ﻿const Datastore = require('nedb')
 const nedbPromise = require('nedb-promise')
 
-const db = new Datastore({autoload: true, filename: 'data/logs/GetUpdatesLog.db'})
+const db = new Datastore({ autoload: true, filename: 'data/logs/GetUpdatesLog.db' })
 const Update = nedbPromise.fromInstance(db)
 
 const compactDb = () => db.persistence.compactDatafile()
 
 const addUpdates = update => Update.insert(update)
 
+const getUpdates = (query = {}) => Update.find(query)
+
+const getUpdatesOne = (query = {}) => Update.findOne(query)
+
 module.exports = {
-    addUpdates
+    addUpdates,
+    getUpdates,
+    getUpdatesOne
 }
