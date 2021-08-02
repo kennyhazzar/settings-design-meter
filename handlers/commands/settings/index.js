3249f9b0ae98162099fb48539f2d25347d183cde
kolchanov <kolchanov@siberian.pro>
2021-08-02 17:09:00 +0000

    Реструктуризация композера

---
+++ handlers/commands/settings/index.js
@@ -0,0 +1,11 @@
+﻿const { Composer } = require('telegraf')
+
+const settings = new Composer()
+
+settings.use(require('./chat/index'))
+
+settings.use(require('./start/index'))
+
+settings.use(require('./text/index'))
+
+module.exports = settings
