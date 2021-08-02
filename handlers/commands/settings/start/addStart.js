08feee7cd9a9e92701f5128bb30b7dc4898da38e
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:04:38 +0000

    Команда для добавления нового шаблона приветственного сообщения

---
+++ handlers/commands/settings/start/addStart.js
@@ -0,0 +1,15 @@
+﻿const Start = require('../../../../stores/context/start.js')
+
+const addStartHandler = async ctx => {
+    var userMessage = ctx.message.text
+
+    const commandInstance = userMessage.substring(userMessage.search(' '), userMessage.length).trim()
+    if (commandInstance === '/addStart') {
+        ctx.reply("Для добавления нового приветственного сообщения как шаблон, введи /addStart <Текст сообщения>")
+    } else {
+        Start.addStart(commandInstance)
+        ctx.reply("Шаблон добавлен, для его выбора ознакомьтесь с командой /selectStart")
+    }
+}
+
+module.exports = addStartHandler
