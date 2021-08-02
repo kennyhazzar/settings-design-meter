ddabfbf4b3ce88ce2d737c100474a94411be0979
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:06:20 +0000

    Команда для выбора текущего шаблона

---
+++ handlers/commands/settings/start/selectStart.js
@@ -0,0 +1,26 @@
+﻿const Start = require('../../../../stores/context/start.js')
+
+const selectStartHandler = async ctx => {
+    var userMessage = ctx.message.text
+
+    const start = await Start.getStart()
+
+    const currentStart = await Start.getCurrentStart()
+
+    const commandInstance = userMessage.substring(userMessage.search(' '), userMessage.length).trim()
+
+    if (commandInstance === '/selectStart') {
+
+        ctx.reply(`Список шаблонов приветственного сообщения:\n(Текст, айди)\n
+        ${start.length == 0 ? "Пусто\n" : start.map((item, index) => {
+            return "\n" + `${index + 1}. ${item.data}\n${item._id}\n`
+        }).join('')}` + `${currentStart ? "\n\nВ данный момент выбран:\n\n" + currentStart.data + 
+        "\n" + currentStart._id : "\n\nВ данный момент ничего не выбрано\n\n"}` +
+        "\n\nДля выбора шаблона, введи /selectStart <айди из списка>")
+    } else {
+        Start.changeCurrentStart(commandInstance)
+        ctx.reply("Шаблон выбран")
+    }
+}
+
+module.exports = selectStartHandler
