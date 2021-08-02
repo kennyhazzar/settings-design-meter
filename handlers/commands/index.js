e046b6c253c6173df78179c3c861d01fd1af31ad
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:19:04 +0000

    Добавлен функционал настройки приветственного сообщения

--- handlers/commands/index.js
+++ handlers/commands/index.js
@@ -6,8 +6,13 @@ const start = require('./start.js')
 const help = require('./help.js')
 const chat = require('./settings/chat/chatAdd.js')
 
+commands.use(require('./settings/start/index'))
+
 commands.command('/chatAdd', chat)
+
 commands.start(start)
+
 commands.help(help)
+
 
 module.exports = commands
