aa43281c3ffccb5bcdc517231b03692ee0f59d2b
kolchanov <kolchanov@siberian.pro>
2021-08-02 17:12:39 +0000

    прикол со стикерами)))

---
+++ handlers/middlewares/stickerxd.js
@@ -0,0 +1,6 @@
+﻿const stickerHandler = ctx => {
+    // ctx.replyWithSticker(ctx.update.message.sticker.file_id)
+    ctx.reply(ctx.update.message.sticker.emoji)
+}
+
+module.exports = stickerHandler
