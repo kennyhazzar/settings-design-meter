011478917750b32a3ce6f818477855402da22003
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:50:56 +0000

    Добавлены html моменты, исправлен баг с отображением текущего выбора

--- handlers/commands/settings/start/showStarts.js
+++ handlers/commands/settings/start/showStarts.js
@@ -9,10 +9,10 @@ const showStartHandler = async ctx => {
 
     const commandInstance = userMessage.substring(userMessage.search(' '), userMessage.length).trim()
     if (commandInstance === '/showStarts') {
-        ctx.reply(`Список шаблонов приветственного сообщения:\n
+        ctx.replyWithHTML(`Список шаблонов приветственного сообщения:\n
         ${start.length == 0 ? "Пусто\n" : start.map((item, index) => {
             return "\n" + `${index + 1}. ${item.data}\n`
-        }).join('')}\n${currentStartData ? "Текущий выбор:\n" + commandInstance : ""}`)
+        }).join('')}\n${currentStartData ? "Текущий выбор:\n" + currentStartData : ""}`)
     }
 }
 
