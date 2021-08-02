56c990e0cbeb5bf33c7573e108f829d4254278bd
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:08:12 +0000

    Сборщик всех команд в композер

---
+++ handlers/commands/settings/start/index.js
@@ -0,0 +1,11 @@
+﻿const { Composer } = require('telegraf')
+
+const start = new Composer()
+
+start.command('/updateStart', require('./updateStart'))
+start.command('/addStart', require('./addStart'))
+start.command('/showStarts', require('./showStarts'))
+start.command('/deleteStart', require('./deleteStart'))
+start.command('selectStart', require('./selectStart'))
+
+module.exports = start
