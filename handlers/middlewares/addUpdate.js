0761a53935f792d3d345791089f1eab38c419f1a
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:16:26 +0000

    Добавлено автоматическое добавление и удаление чата из бд при добавлении/удаления бота в/из чат/a

--- handlers/middlewares/addUpdate.js
+++ handlers/middlewares/addUpdate.js
@@ -1,8 +1,18 @@
 ﻿const { addUpdates } = require('../../stores/logs/getUpdatesLog.js')
-
+const { addChat, removeChatByChatId } = require('../../stores/context/chat.js')
 const addUpdate = async (ctx, next) => {
-    console.log((await ctx.telegram.getUpdates())[0])
-    addUpdates((await ctx.telegram.getUpdates())[0])
+    const update = (await ctx.telegram.getUpdates())[0]
+    addUpdates(update)
+    if (update?.message?.group_chat_created == true) {
+        console.log(update?.message?.chat.id)
+        addChat({ chat: update?.message?.chat }, update?.message?.chat.id, true)
+    }
+    if (update?.my_chat_member?.new_chat_member?.status == 'left' ||
+        update?.my_chat_member?.old_chat_member?.status == 'left') {
+        const deletedChatId = update?.my_chat_member?.chat?.id
+        console.log(deletedChatId)
+        removeChatByChatId({ chatId: deletedChatId })
+    }
     return next()
 }
 
