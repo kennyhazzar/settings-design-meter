b0e320445dd07448998eb67451aa9a6b7c4b6f0c
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:05:47 +0000

    Команда для удаления шаблона из бд по id

---
+++ handlers/commands/settings/start/deleteStart.js
@@ -0,0 +1,22 @@
+﻿const Start = require('../../../../stores/context/start.js')
+
+const deleteStartHandler = async ctx => {
+    var userMessage = ctx.message.text
+
+    const start = await Start.getStart()
+
+    const commandInstance = userMessage.substring(userMessage.search(' '), userMessage.length).trim()
+
+    if (commandInstance === '/deleteStart') {
+
+        ctx.reply(`Список шаблонов приветственного сообщения:\n(Текст, айди)\n
+        ${start.length == 0 ? "Пусто\n" : start.map((item, index) => {
+            return "\n" + `${index + 1}. ${item.data}\n${item._id}\n`
+        }).join('')}\n\nДля удаления шаблона, введи /deleteStart <айди из списка>`)
+    } else {
+        Start.removeStart(commandInstance)
+        ctx.reply("Шаблон удален")
+    }
+}
+
+module.exports = deleteStartHandler
