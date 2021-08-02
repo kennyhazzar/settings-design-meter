6da771d79ff349990a79d533989a8b579ae82bc3
kolchanov <kolchanov@siberian.pro>
2021-08-02 04:50:04 +0000

    Убрал лишние слеши, добавил новую команду /startHelp

--- handlers/commands/settings/start/index.js
+++ handlers/commands/settings/start/index.js
@@ -2,10 +2,11 @@
 
 const start = new Composer()
 
-start.command('/updateStart', require('./updateStart'))
-start.command('/addStart', require('./addStart'))
-start.command('/showStarts', require('./showStarts'))
-start.command('/deleteStart', require('./deleteStart'))
+start.command('updateStart', require('./updateStart'))
+start.command('addStart', require('./addStart'))
+start.command('showStarts', require('./showStarts'))
+start.command('deleteStart', require('./deleteStart'))
 start.command('selectStart', require('./selectStart'))
+start.command('startHelp', require('./startHelp'))
 
 module.exports = start
