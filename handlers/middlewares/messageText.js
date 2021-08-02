cea2947e2f6176b17d49f4148159f7505b07d784
kolchanov <kolchanov@siberian.pro>
2021-08-02 07:16:28 +0000

    Исправлена ошибка с получением данных с бд - добавлена асинхронность

--- handlers/middlewares/messageText.js
+++ handlers/middlewares/messageText.js
@@ -1,9 +1,12 @@
 ﻿const MessageText = require('../../stores/context/voidMessage.js')
 
-const messageHandler = ctx => {
+const messageHandler = async ctx => {
     if (ctx.chat.type !== 'private') return null
-    const message = MessageText.getCurrentMessage()
-    if (Object.keys(message).length == 0) return ctx.reply("Бот не настроен: ответ на текст.")
+
+    const message = await MessageText.getCurrentMessage()
+
+    if (!message) return ctx.reply("Я тебя не понимаю")
+    
     ctx.reply(message.data)
 }
 
