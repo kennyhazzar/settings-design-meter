4c02f369fc009026666285da7df0a2595b4111a4
kolchanov <kolchanov@siberian.pro>
2021-08-02 17:11:41 +0000

    Часть реструктуризации комманд

--- handlers/commands/index.js
+++ handlers/commands/index.js
@@ -2,19 +2,10 @@
 
 const commands = new Composer()
 
-const start = require('./start.js')
-const help = require('./help.js')
-const chat = require('./settings/chat/chatAdd.js')
+commands.start(require('./start.js'))
 
-commands.use(require('./settings/start/index'))
+commands.help(require('./help.js'))
 
-commands.use(require('./settings/text/index'))
-
-commands.command('/chatAdd', chat)
-
-commands.start(start)
-
-commands.help(help)
-
+commands.use(require('./settings/index'))
 
 module.exports = commands
