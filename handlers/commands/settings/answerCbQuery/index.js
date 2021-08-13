97941b66a72374d8d9959fb18476dd3b9dfc61ec
kolchanov <56494597+kennyhazzar@users.noreply.github.com>
2021-08-13 18:16:38 +0000

    Добавлены новые настройки cbQuery

--- handlers/commands/settings/answerCbQuery/index.js
+++ handlers/commands/settings/answerCbQuery/index.js
@@ -4,6 +4,14 @@ const query = new Composer()
 
 query.use(require('./addQuery.js'))
 
+query.use(require('./updateQuery.js'))
+
 query.command("selectQuery", require('./selectQuery.js'))
+
+query.command("queryHelp", require('./queryHelp.js'))
+
+query.command("deleteQuery", require('./deleteQuery.js'))
+
+query.command("showQueries", require('./showQueries.js'))
 
 module.exports = query
