3567a5c685e50041785a51e51e0399570e82ea11
kolchanov <56494597+kennyhazzar@users.noreply.github.com>
2021-08-13 07:21:55 +0000

    Обновлены импорты и добавлен обработчик для session

--- bot/index.js
+++ bot/index.js
@@ -1,11 +1,14 @@
 ﻿const ms = require('millisecond')
-const { Telegraf } = require('telegraf')
+const { session, Telegraf } = require('telegraf')
+
 const config = require('config')
 
 const bot = new Telegraf(config.get('botToken'), {
     handlerTimeout: ms('5s'),
     telegram: { webhookReply: false }
 })
+
+bot.use(session())
 
 module.exports = bot
 
