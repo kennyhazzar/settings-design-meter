8745fd43fbd88f9b4136af0ccfec9353b9d00ac6
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2021-12-25 05:18:55 +0700

    simple function for getting callback tag

---
+++ helpers/index.js
@@ -0,0 +1,16 @@
+﻿const getCallbackData = index => {
+    if (index == 1) {
+        return "Hot"
+    }
+    if (index == 2) {
+        return "Warm"
+    }
+    if (index == 3) {
+        return "Unclear"
+    }
+    if (index == 4) {
+        return "Cold"
+    }
+}
+
+module.exports = { getCallbackData }
